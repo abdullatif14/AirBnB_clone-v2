c09901fc96a09b00c2960f53aa2b635bd3ca0d11
abdullatif14 <abdilatifmohamed345@gmail.com>
2023-03-24 00:09:28 -0700

    Updated Place

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,7 +13,7 @@ from models.user import User
 from os import getenv
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.place import place_amenity
+    from models.place import place
 
 classes = {"User": User, "State": State, "City": City,
            "Amenity": Amenity, "Place": Place, "Review": Review}
